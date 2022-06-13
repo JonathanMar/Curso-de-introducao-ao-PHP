2a9e934567912891e0208bf333026daeb9f0d026
Jonathan Marcon <jonathanmarcom4@gmail.com>
2022-06-12 22:12:23 -0300

    .

--- index.php
+++ index.php
@@ -15,33 +15,33 @@
         <h2> Curso de introdução ao PHP </h2> 
     </header>
 
-    <main> 
+    <main> \
         <h3>- Índice -</h3>
 
         <div class="aulas">
         <ul>
-            <li><a href="arquivos/01-fundamentos_basicos.php"> Fundamentos Básicos </a></li>
-            <li><a href="arquivos/02-variaveis.php"> Variáveis </a></li>
-            <li><a href="arquivos/03-variaveis_variaveis.php"> Variáveis Variáveis </a></li>
-            <li><a href="arquivos/04-tipos_de_dados.php"> Tipos de dados </a></li>
-            <li><a href="arquivos/05-aspas_concatenacao.php"> Aspas Concatenação </a></li>
-            <li><a href="arquivos/06-escopo_de_variaveis.php"> Escopo de variáveis </a></li>
-            <li><a href="arquivos/07-constantes.php"> Constantes </a></li>
-            <li><a href="arquivos/08-arrays.php"> Arrays </a></li>
-            <li><a href="arquivos/09-funcoes_de_arrays.php"> Funções de arrays </a></li>
-            <li><a href="arquivos/10-condicionais.php"> Condicionais </a></li>
-            <li><a href="arquivos/11-operadores-aritimeticos.php"> Operadores aritméticos </a></li>
-            <li><a href="arquivos/13-operadores-de-atribuicao.php"> Operadores de atribuição </a></li>
-            <li><a href="arquivos/14-operadores-de-comparacao.php"> Operadores de comparação </a></li>
-            <li><a href="arquivos/15-operadores-logicos.php"> Operadores Lógicos </a></li>
-            <li><a href="arquivos/16-while-dowhile.php"> While & Dowhile </a></li>
-            <li><a href="arquivos/17-for-foreach.php"> For Foreach </a></li>
-            <li><a href="arquivos/18-funcoes-para-strings.php"> Funções para Strings </a></li>
-            <li><a href="arquivos/20-criando-funcao.php"> Funções para Números </a></li>
-            <li><a href="arquivos/21-superglobais.php"> Super Globais </a></li>
-            <li><a href="arquivos/22-formularios/index.php"> Formulário </a></li>
-            <li><a href="arquivos/22-formularios/validacao.php"> Validação de dados </a></li>
-            <li><a href="arquivos/23-upload/index.php"> Upload de arquivos </a></li>
+            <li><a href="arquivos/aulas/02-variaveis.php"> Variáveis </a></li>
+            <li><a href="arquivos/aulas/03-variaveis_variaveis.php"> Variáveis Variáveis </a></li>
+            <li><a href="arquivos/aulas/01-fundamentos_basicos.php"> Fundamentos Básicos </a></li>
+            <li><a href="arquivos/aulas/04-tipos_de_dados.php"> Tipos de dados </a></li>
+            <li><a href="arquivos/aulas/05-aspas_concatenacao.php"> Aspas Concatenação </a></li>
+            <li><a href="arquivos/aulas/06-escopo_de_variaveis.php"> Escopo de variáveis </a></li>
+            <li><a href="arquivos/aulas/07-constantes.php"> Constantes </a></li>
+            <li><a href="arquivos/aulas/08-arrays.php"> Arrays </a></li>
+            <li><a href="arquivos/aulas/09-funcoes_de_arrays.php"> Funções de arrays </a></li>
+            <li><a href="arquivos/aulas/10-condicionais.php"> Condicionais </a></li>
+            <li><a href="arquivos/aulas/11-operadores-aritimeticos.php"> Operadores aritméticos </a></li>
+            <li><a href="arquivos/aulas/13-operadores-de-atribuicao.php"> Operadores de atribuição </a></li>
+            <li><a href="arquivos/aulas/14-operadores-de-comparacao.php"> Operadores de comparação </a></li>
+            <li><a href="arquivos/aulas/15-operadores-logicos.php"> Operadores Lógicos </a></li>
+            <li><a href="arquivos/aulas/16-while-dowhile.php"> While & Dowhile </a></li>
+            <li><a href="arquivos/aulas/17-for-foreach.php"> For Foreach </a></li>
+            <li><a href="arquivos/aulas/18-funcoes-para-strings.php"> Funções para Strings </a></li>
+            <li><a href="arquivos/aulas/20-criando-funcao.php"> Funções para Números </a></li>
+            <li><a href="arquivos/aulas/21-superglobais.php"> Super Globais </a></li>
+            <li><a href="arquivos/aulas/22-formularios/index.php"> Formulário </a></li>
+            <li><a href="arquivos/aulas/22-formularios/validacao.php"> Validação de dados </a></li>
+            <li><a href="arquivos/aulas/23-upload/index.php"> Upload de arquivos </a></li>
         </ul>
     </main>
 </div>
